725dbd255bbef9fdbdc6835b869d76fa4a590a80
Lucas Batista Pereira <lucas.bpereira1999@gmail.com>
2025-09-28 19:29:49 -0300

    Refactor: Whole Backend

--- backend/agents/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Agents module - Contains all the AI agents for the chatbot system.
-"""
-from .base_agent import BaseAgent
-from .router_agent import RouterAgent
-from .knowledge_agent import KnowledgeAgent
-from .math_agent import MathAgent
-
-__all__ = ['BaseAgent', 'RouterAgent', 'KnowledgeAgent', 'MathAgent']
